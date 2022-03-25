4a7c571c5542851e840ba42f7df55b4dd7b24928
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-03-25 12:15:34 -0700

    Remove dependence on Django and Django REST Framework, Part II

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'django>=3.2.0',
-    'djangorestframework>=3.12.0',
     'elasticsearch>=7.0.0,<8.0.0',
     'elasticsearch-dsl>=7.0.0,<8.0.0',
 ]
