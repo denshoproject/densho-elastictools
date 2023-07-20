d78e460e16eec2ee872c5efbfd7b3459fecb5b7d
Geoffrey Jost <geoffrey.jost@densho.org>
2023-07-20 10:42:45 -0700

    VERSION: 1.1.0 (fixed setup.py)

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/denshoproject/densho-elastictools',
-    version='1.0.2',
+    version='1.1.0',
     zip_safe=False,
 )
